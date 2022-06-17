afb4f99647f9ef13ad2e402b4f83705ce3097410
Dimas <31516927+dimas-prates@users.noreply.github.com>
2022-06-16 22:16:49 -0300

    :sparkles: feat(server/app): Create server/app with basic route

---
+++ src/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const port = 3000;
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+// app.get("/", (req, res) => {
+//   res.send("Server UP");
+// });
+
+require("./controllers/authController")(app);
+app.listen(port);
