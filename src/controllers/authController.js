9ee96b27158359d5603b04c6171131a4f2fa1491
Dimas <31516927+dimas-prates@users.noreply.github.com>
2022-06-16 23:34:00 -0300

    :sparkles: feat(authController): add library/framework bcrypt + jwt. Configured function to generate token + route behavior

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,6 +1,20 @@
 const express = require("express");
+// Crypt/Decrypt password
+const bcrypt = require("bcryptjs");
+// Generate pass token
+const jwt = require("jsonwebtoken");
+//Importing random hash
+const authConfig = require("../config/auth.json");
+// data from db to manipulate
 const User = require("../models/User");
+// Controll/Indicate router
 const router = express.Router();
+
+function generateToken(params = {}) {
+  return jwt.sign(params, authConfig.secret, {
+    expiresIn: 86400,
+  });
+}
 router.post("/register", async (req, res) => {
   const { email } = req.body;
   try {
@@ -9,10 +23,30 @@ router.post("/register", async (req, res) => {
     }
     const user = await User.create(req.body);
     user.password = undefined;
-    return res.send({ user });
+    // return res.send({ user });
+    return res.send({ user, token: generateToken({ id: user.id }) });
   } catch (err) {
     return res.status(400).send({ error: "Registration Failed" });
   }
 });
+router.post("/athenticate", async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email }).select("+password");
 
+  if (!user) {
+    return res.status(400).send({ error: "User not found" });
+  }
+  if (!(await bcrypt.compare(password, user.password))) {
+    return res.status(400).send({ erro: "Invalid password" });
+  }
+  // removed pass for reponse
+  user.password = undefined;
+
+  // generate token
+  // const token = jwt.sign({ id: user.id }, authConfig.secret, {
+  //   expiresIn: 86400,
+  // });
+  // res.send({ user, token });
+  res.send({ user, token: generateToken({ id: user.id }) });
+});
 module.exports = (app) => app.use("/auth", router);
