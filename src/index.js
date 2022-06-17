b58d86400b6c2ad9a7e56559cacb66eb2bfd777e
Dimas <31516927+dimas-prates@users.noreply.github.com>
2022-06-16 23:38:30 -0300

    :sparkles: feat(index.js): Add log for starting the server/app

--- src/index.js
+++ src/index.js
@@ -10,4 +10,4 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 require("./controllers/authController")(app);
 require("./controllers/projectController")(app);
-app.listen(port);
+app.listen(port,() => {console.log('Server is running...')});
