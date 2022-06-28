8cbe39f132215671827a413057b5288e824495d8
Dimas <31516927+dimas-prates@users.noreply.github.com>
2022-06-28 20:30:36 -0300

    :sparkles: feat(projectController): Finished functions for put route

--- src/app/controllers/projectController.js
+++ src/app/controllers/projectController.js
@@ -8,7 +8,7 @@ router.use(authMiddleware);
 
 router.get("/", async (req, res) => {
   try {
-    const projects = await Project.find().populate("user");
+    const projects = await Project.find().populate(["user", "tasks"]);
     return res.send({ projects });
   } catch (err) {
     return res.status(400).send({ error: "Error loading projects" });
@@ -18,9 +18,10 @@ router.get("/", async (req, res) => {
 router.get("/:projectId", async (req, res) => {
   // res.send({ user: req.userId });
   try {
-    const project = await Project.findById(req.params.projectId).populate(
-      "user"
-    );
+    const project = await Project.findById(req.params.projectId).populate([
+      "user",
+      "tasks",
+    ]);
     return res.send({ project });
   } catch (err) {
     return res.status(400).send({ error: "Error loading project" });
@@ -62,7 +63,35 @@ router.post("/", async (req, res) => {
 });
 
 router.put("/:projectId", async (req, res) => {
-  res.send({ user: req.userId });
+  // res.send({ user: req.userId });
+  try {
+    const { title, description, tasks } = req.body;
+    const project = await Project.findByIdAndUpdate(
+      req.params.projectId,
+      {
+        title,
+        description,
+      },
+      { new: true }
+    );
+    project.tasks = [];
+    await Task.remove({ project: project._id });
+
+    await Promise.all(
+      tasks.map(async (task) => {
+        const projectTask = new Task({ ...task, project: project._id });
+        await projectTask.save();
+        project.tasks.push(projectTask);
+      })
+    );
+
+    await project.save();
+    return res.send({ project });
+  } catch (err) {
+    // console.log(err);
+    // console.log(tasks);
+    return res.status(400).send({ error: "Error updating project" });
+  }
 });
 
 router.delete("/:projectId", async (req, res) => {
